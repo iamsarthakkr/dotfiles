7d2872c4077d7f9098d2f89bc29c3e6053e79dea
sarthak kumar <sarthakkumar10151@gmail.com>
2025-07-01 00:41:29 +0530

    changes to configs

--- .config/nvim/lua/user/plugins/which-key.lua
+++ .config/nvim/lua/user/plugins/which-key.lua
@@ -1,15 +1,10 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 5000
-	end,
 	opts = {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
-		delay = 5000,
 	},
 	keys = {
 		{
@@ -20,8 +15,4 @@ return {
 			desc = "Buffer Local Keymaps (which-key)",
 		},
 	},
-	config = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 5000
-	end
 }
